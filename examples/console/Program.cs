904a7567e6d0e103073324d690fc1ba4ae36dd77
Boris Wilhelms <mail@wille-zone.de>
2023-04-03 18:19:43 +0200

    chore: Add examples

---
+++ examples/console/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+var client = new HttpClient();
+var response = await client.GetStringAsync("https://localhost:7189");
+Console.WriteLine(response);
